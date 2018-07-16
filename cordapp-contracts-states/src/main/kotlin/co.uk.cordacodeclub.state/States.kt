798e966faf9650c5ba231097dfcd112c02aa2f0e
Viv Paranjape <vivasvanp@gmail.com>
2018-07-16 19:52:57 +0100

    fixed name

--- cordapp-contracts-states/src/main/kotlin/co.uk.cordacodeclub.state/States.kt
+++ cordapp-contracts-states/src/main/kotlin/co.uk.cordacodeclub.state/States.kt
@@ -1,6 +1,6 @@
 // *********
 // * State *
 // *********
-data class TemplateState(val data: String, val nino: String, val operation: Boolean, val amount: Int, val creationDate: String) : ContractState {
+data class StatementState(val data: String, val nino: String, val operation: Boolean, val amount: Int, val creationDate: String) : ContractState {
     override val participants: List<AbstractParty> get() = listOf()
 }
