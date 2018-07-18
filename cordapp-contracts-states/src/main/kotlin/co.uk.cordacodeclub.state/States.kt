46b444a6d18cc356f06eeef87d577d0c4121848d
Smilen <smilen.stoychev@redington.co.uk>
2018-07-18 14:45:38 +0100

    Added Ownable state with a very, very generic state

--- cordapp-contracts-states/src/main/kotlin/co.uk.cordacodeclub.state/States.kt
+++ cordapp-contracts-states/src/main/kotlin/co.uk.cordacodeclub.state/States.kt
@@ -1,6 +1,34 @@
+import net.corda.core.contracts.CommandAndState
+import net.corda.core.contracts.OwnableState
+import net.corda.core.identity.AbstractParty
+import java.util.*
+
 // *********
 // * State *
 // *********
-data class StatementState(val data: String, val nino: String, val operation: Boolean, val amount: Int, val creationDate: String) : ContractState {
+data class StatementState(override val owner :AbstractParty,
+                          val nino: String,
+                          val dateReported: Date,
+                          val accountType: AccountType,
+                          val accountNumber: String?,
+                          val dateAccountOpened: Date?,
+                          val amountOrLastBalance: Double?,
+                          val amountOverdue: Double?,
+                          val originalCreditAmountOrLimit: Double?,
+                          val responsibility: Responsibility = Responsibility.INDIVIDUAL
+) : OwnableState {
+    override fun withNewOwner(newOwner: AbstractParty): CommandAndState {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override val participants: List<AbstractParty> get() = listOf()
+}
+
+
+enum class AccountType {
+    DEBIT, CREDIT, MORTGAGE, AUTOLOAN,
+}
+
+enum class Responsibility{
+    INDIVIDUAL, JOINT
 }
