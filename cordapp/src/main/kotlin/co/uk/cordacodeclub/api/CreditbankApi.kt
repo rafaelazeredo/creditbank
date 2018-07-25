a6c89166fd8c49d491e04af5f3730390d0958ac5
Bartman250 <mark.simpson@rbs.com>
2018-07-25 17:56:56 +0100

    rest endpoint for credit

---
+++ cordapp/src/main/kotlin/co/uk/cordacodeclub/api/CreditbankApi.kt
@@ -0,0 +1,21 @@
+package co.uk.cordacodeclub.api
+
+import net.corda.core.node.AppServiceHub
+import net.corda.core.serialization.CordaSerializable
+
+@CordaSerializable
+class CustomerTransaction(val amount : Int, val description : String,val debit : Boolean)
+
+
+interface CreditBankApi {
+  fun getLastTransaction() : CustomerTransaction
+  fun addTransaction(customerTransaction: CustomerTransaction)
+}
+
+class CreditbankApiImpl(serviceHub: AppServiceHub) : CreditBankApi {
+  override fun getLastTransaction() : CustomerTransaction {
+    return CustomerTransaction(10,"mobile phone top up", true) }
+  override fun addTransaction(customerTransaction: CustomerTransaction) {
+    println("Gonna stick stuff in the ledger $customerTransaction")
+  }
+}
