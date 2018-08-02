d52355628d8f6f6c161a5fdbd658fb7e6d789ee9
Joe Thomas <joseph.thomas@bluebank.io>
2018-08-02 22:07:32 +0100

    wrote RequestStatementFlow, added Request State & Contract

--- cordapp-contracts-states/src/main/kotlin/co/uk/cordacodeclub/contract/Contracts.kt
+++ cordapp-contracts-states/src/main/kotlin/co/uk/cordacodeclub/contract/Contracts.kt
@@ -22,4 +22,23 @@ class StatementContract : Contract {
         class AddParticipant : Commands
     }
 }
+class RequestContract : Contract {
+
+    companion object {
+        @JvmStatic
+        val STATEMENT_CONTRACT_ID = "co.uk.cordacodeclub.contract.RequestContract"
+    }
+
+    // A transaction is considered valid if the verify() function of the contract of each of the transaction's input
+    // and output states does not throw an exception.
+    override fun verify(tx: LedgerTransaction) {
+        // Verification logic goes here.
+    }
+//
+//    // Used to indicate the transaction's intent.
+//    interface Commands : CommandData {
+//        class Issue : Commands
+//        class AddParticipant : Commands
+//    }
+}
 
