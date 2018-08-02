7a706d9ac01ca55f3323fca3f2f41c09e15d859f
Joe Thomas <joseph.thomas@bluebank.io>
2018-08-02 19:41:27 +0100

    wrote issue flow, modified customerTransaction object

--- cordapp/src/main/kotlin/co/uk/cordacodeclub/flows/IssueStatementFlow.kt
+++ cordapp/src/main/kotlin/co/uk/cordacodeclub/flows/IssueStatementFlow.kt
@@ -1,1 +1,93 @@
 package co.uk.cordacodeclub.flows
+import co.paralleluniverse.fibers.Suspendable
+import co.uk.cordacodeclub.api.CustomerTransaction
+import co.uk.cordacodeclub.contract.StatementContract
+import co.uk.cordacodeclub.state.AccountType
+import co.uk.cordacodeclub.state.Responsibility
+import co.uk.cordacodeclub.state.StatementState
+import net.corda.core.contracts.Command
+import net.corda.core.contracts.StateAndRef
+import net.corda.core.flows.FinalityFlow
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.InitiatingFlow
+import net.corda.core.flows.StartableByRPC
+import net.corda.core.identity.Party
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.transactions.TransactionBuilder
+import java.sql.Statement
+import java.util.*
+
+@InitiatingFlow
+@StartableByRPC
+class IssueStatementFlow(val customerTransaction : CustomerTransaction) : FlowLogic<SignedTransaction>() {
+
+    // FLOW WILL RECIEVER TRANSACTION OBJECT
+    // CREATE STATE FORM OBJECT
+    // CREATE TRANSACTION BUILDER
+    // ADD STORE IN VAULT COMMAND TO TRANSACTION
+    // SIGN TRANSACTION
+    // FINALITY FLOW
+
+
+    @Suspendable
+    override fun call(): SignedTransaction {
+
+        val state = initialiseStatementState()
+
+        val txBuilder = createTransactionBuilder(state)
+
+        val fullySignedTransaction = serviceHub.signInitialTransaction(txBuilder)
+
+        return subFlow(FinalityFlow(fullySignedTransaction))
+    }
+
+    @Suspendable
+    private fun initialiseStatementState(): StatementState {
+
+        val myParty = serviceHub.myInfo.legalIdentities[0]
+
+        // some of this data that is currently null would either be included in data received or taken from accounts DB
+        return StatementState.issue(owner = myParty,
+                nino = customerTransaction.nino,
+                dateReported = customerTransaction.date,
+                accountType = customerTransaction.accountType,
+                accountNumber = null,
+                dateAccountOpened = null,
+                amountOrLastBalance = customerTransaction.amount,
+                amountOverdue = null,
+                originalCreditAmountOrLimit = null)
+    }
+
+    @Suspendable
+    private fun createTransactionBuilder(state: StatementState): TransactionBuilder {
+        var txBuilder = TransactionBuilder(getNotary())
+
+        addOutputStateToTransaction(state, txBuilder)
+
+        val addStoreStatementCommand = createStoreStatementCommand()
+
+        txBuilder.addCommand(addStoreStatementCommand)
+
+        return txBuilder
+    }
+
+    @Suspendable
+    private fun getNotary(): Party {
+        val notary = serviceHub.networkMapCache.notaryIdentities.first();
+        return notary
+    }
+
+    @Suspendable
+    private fun createStoreStatementCommand(): Command<StatementContract.Commands.Issue> {
+        val signer = ourIdentity.owningKey
+        val storeStatementCommand = Command(StatementContract.Commands.Issue(), signer)
+        return storeStatementCommand
+    }
+
+    @Suspendable
+    private fun addOutputStateToTransaction(state: StatementState, txBuilder: TransactionBuilder) {
+            txBuilder.addOutputState(state, StatementContract.STATEMENT_CONTRACT_ID)
+    }
+
+
+}
