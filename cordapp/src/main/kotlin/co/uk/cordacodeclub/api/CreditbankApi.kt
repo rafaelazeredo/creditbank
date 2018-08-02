4ee484e55d978894a9a93195dd0586a3ff412fcd
Joe Thomas <joseph.thomas@bluebank.io>
2018-08-02 19:41:27 +0100

    wrote issue flow, modified customerTransaction object

--- cordapp/src/main/kotlin/co/uk/cordacodeclub/api/CreditbankApi.kt
+++ cordapp/src/main/kotlin/co/uk/cordacodeclub/api/CreditbankApi.kt
@@ -1,10 +1,12 @@
 package co.uk.cordacodeclub.api
 
+import co.uk.cordacodeclub.state.AccountType
 import net.corda.core.node.AppServiceHub
 import net.corda.core.serialization.CordaSerializable
+import java.util.Date
 
 @CordaSerializable
-class CustomerTransaction(val amount : Int, val description : String,val debit : Boolean)
+class CustomerTransaction(val nino : String, val date: Date, val accountType: AccountType, val amount : Double, val description : String,val debit : Boolean)
 
 interface CreditBankApi {
   fun getLastTransaction() : CustomerTransaction
@@ -13,7 +15,7 @@ interface CreditBankApi {
 
 class CreditbankApiImpl(serviceHub: AppServiceHub) : CreditBankApi {
   override fun getLastTransaction() : CustomerTransaction {
-    return CustomerTransaction(10,"mobile phone top up", true) }
+    return CustomerTransaction("NB808883D", java.util.Date(), AccountType.DEBIT, 10.0,"mobile phone top up", true) }
   override fun addTransaction(customerTransaction: CustomerTransaction) {
     println("Gonna stick stuff in the ledger $customerTransaction")
   }
