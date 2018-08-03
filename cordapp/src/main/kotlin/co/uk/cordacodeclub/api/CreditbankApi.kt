ee4bb18d36f07afbd7461ec40cc9196bc2c9872b
Joe Thomas <joseph.thomas@bluebank.io>
2018-08-03 15:17:28 +0100

    initial commit

--- cordapp/src/main/kotlin/co/uk/cordacodeclub/api/CreditbankApi.kt
+++ cordapp/src/main/kotlin/co/uk/cordacodeclub/api/CreditbankApi.kt
@@ -4,19 +4,29 @@ import co.uk.cordacodeclub.state.AccountType
 import net.corda.core.node.AppServiceHub
 import net.corda.core.serialization.CordaSerializable
 import java.util.Date
+import net.corda.core.flows.FlowLogic
 
 @CordaSerializable
 class CustomerTransaction(val nino : String, val date: Date, val accountType: AccountType, val amount : Double, val description : String,val debit : Boolean)
 
 interface CreditBankApi {
-  fun getLastTransaction() : CustomerTransaction
-  fun addTransaction(customerTransaction: CustomerTransaction)
+    fun getLastTransaction() : CustomerTransaction
+    fun addTransaction(customerTransaction: CustomerTransaction)
 }
 
 class CreditbankApiImpl(serviceHub: AppServiceHub) : CreditBankApi {
-  override fun getLastTransaction() : CustomerTransaction {
-    return CustomerTransaction("NB808883D", java.util.Date(), AccountType.DEBIT, 10.0,"mobile phone top up", true) }
-  override fun addTransaction(customerTransaction: CustomerTransaction) {
-    println("Gonna stick stuff in the ledger $customerTransaction")
-  }
+
+    override fun getLastTransaction() : CustomerTransaction {
+        return CustomerTransaction("NB808883D",
+                                   java.util.Date(),
+                                   AccountType.DEBIT,
+                                   10.0,
+                                   "mobile phone top up",
+                                   true)
+    }
+
+    override fun addTransaction(customerTransaction: CustomerTransaction) {
+        println("Gonna stick stuff in the ledger $customerTransaction")
+        service
+    }
 }
